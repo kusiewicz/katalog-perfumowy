c2f612f295e4680ee4cba95d4b769d8a59d58479
kusiewicz <kamil.kusy111@gmail.com>
2021-08-24 13:39:24 +0200

    package.json for heroku created

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
 
 const path = require('path');
 
@@ -10,11 +9,10 @@ app.get('/essa', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
 });
 
-
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server now listening at http://localhost:${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running...');
 });
